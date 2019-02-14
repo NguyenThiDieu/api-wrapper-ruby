1a6d563df8c9a00fbbc45ee58d47118981ec6f26
NguyenThiDieu <nguyen.thi.dieu@framgia.com>
2019-02-14 09:36:15 +0700

    222473_add_storage

--- lib/uiza.rb
+++ lib/uiza.rb
@@ -5,12 +5,14 @@ require "uiza/version"
 require "uiza/uiza_client"
 
 require "uiza/api_operations/create"
+require "uiza/api_operations/add"
 require "uiza/api_operations/retrieve"
 require "uiza/api_operations/list"
 require "uiza/api_operations/update"
 require "uiza/api_operations/delete"
 
 require "uiza/entity"
+require "uiza/storage"
 
 module Uiza
   class << self
---
+++ lib/uiza/storage.rb
@@ -0,0 +1,7 @@
+module Uiza
+  class Storage
+    extend Uiza::APIOperations::Add
+
+    OBJECT_API_PATH = "media/storage".freeze
+  end
+end
